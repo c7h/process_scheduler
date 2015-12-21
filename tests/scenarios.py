03608263f2054a34ee8f68e763b0d8f657db060d
Christoph Gerneth <login@gerneth.info>
2015-12-21 15:35:22 +0100

    stage 2: implementing Multilevel-strategies. starting with tests

--- tests/scenarios.py
+++ tests/scenarios.py
@@ -1,9 +1,9 @@
 import unittest
-
 from process.manager import ProcessManager
 from process.process import Process, PCB, Workplan
 from scheduler.timer import SystemTimer
 from scheduler.core import SchedulerFactory
+
 
 # Scenario collections for unittests
 
@@ -31,7 +31,6 @@ class SimpleFiFoScenario(unittest.TestCase):
         p2.workplan = Workplan().work(10)
         p1.workplan = Workplan().work(10).launch('B').wait(15).work(10)
 
-
     def tearDown(self):
         ProcessManager._drop()
         SystemTimer._drop()
@@ -43,6 +42,44 @@ class FiFoScenario2(unittest.TestCase):
         pcb2 = PCB(Process("B"))
         pcb1.process.workplan = Workplan().work(10).launch("B").work(10).wait(10).work(5)
         pcb2.process.workplan = Workplan().work(15)
+
+    def tearDown(self):
+        ProcessManager._drop()
+        SystemTimer._drop()
+
+
+class MLScenario1(unittest.TestCase):
+    """
+    A(1):Work(10),Launch(B),Work(10)
+    B(2):Work(10),Launch(C),Work(10)
+    C(3):Work(10),Launch(D),Work(10)
+    D(4):Work(10),Launch(E),Work(10)
+    E(5):Work(10),Launch(F),Work(10)
+    F(6):Work(10)
+
+    should result in a funny scenario!
+    (here: MLsndFiFo)
+
+    A(1):--                  --
+    B(2):  --              --
+    C(3):    --          --
+    D(4):      --      --
+    E(5):        --  --
+    F(6):          --
+    """
+
+    def setUp(self):
+        names = ['F', 'E', 'D', 'C', 'B', 'A']
+        prios = [6, 5, 4, 3, 2, 1]
+        former_pcb = None
+        for n, p in zip(names, prios):
+            process = Process(name=n)
+            pcb = PCB(process, prio=p)
+            if former_pcb != None:
+                wp = Workplan().work(10).launch(former_pcb)
+            else:
+                wp = Workplan().work(10)  # last workplan does't need to launch a pcb
+            pcb.process.workplan = wp
 
     def tearDown(self):
         ProcessManager._drop()
--- tests/test_strategy_concrete.py
+++ tests/test_strategy_concrete.py
@@ -1,7 +1,9 @@
 import unittest
-from scenarios import BaseScenario
+from scenarios import BaseScenario, MLScenario1
+
 from scheduler.core import Scheduler
 from strategy.simple import FiFo, RoundRobin
+from strategy.multilevel import MLsecondFiFo
 from common.evaluator import StrategyEvaluator, ProcessEvaluator
 
 
@@ -38,12 +40,12 @@ class RRStrategyTestCase(BaseScenario):
     def test_RR_t10_q1(self):
         self._run_rr(quantum=1, timeslice=10)
         start, end = self.pe.getPeriodForPCB("FOO")
-        self.assertEqual(end, 30) # work(10),launch(BAR),ready(10),work(10) -> 30
+        self.assertEqual(end, 30)  # work(10),launch(BAR),ready(10),work(10) -> 30
 
     def test_RR_t10_q2(self):
         self._run_rr(quantum=2, timeslice=10)
         start, end = self.pe.getPeriodForPCB("FOO")
-        self.assertEqual(end, 20) # work(10),launch(BAR),work(10) -> 20
+        self.assertEqual(end, 20)  # work(10),launch(BAR),work(10) -> 20
 
     def test_RR_t5_q1(self):
         self._run_rr(quantum=1, timeslice=5)
@@ -54,6 +56,13 @@ class RRStrategyTestCase(BaseScenario):
         self._run_rr(quantum=2, timeslice=5)
         start, end = self.pe.getPeriodForPCB("FOO")
         self.assertTupleEqual((0, 30), (start, end))
+
+class MultilevelPriorityFiFo(MLScenario1):
+    def test_MLsndFiFo_01(self):
+        strategy = MLsecondFiFo()
+        scheduler = Scheduler(strategy=strategy)
+        scheduler.initialize("A")
+        scheduler.run()
 
 
 if __name__ == '__main__':
